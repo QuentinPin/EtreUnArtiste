262d88d4b804a01ed8f4f5fec5cec6dc66c7d0f5
pineauque <quentin_pineau@reseau.eseo.fr>
2020-05-07 17:12:35 +0200

    Validation de la partie 3.5.1.1 par l'acsm : interface Coloriable.java

---
+++ src/fr/eseo/poo/projet/artiste/modele/Coloriable.java
@@ -0,0 +1,10 @@
+package fr.eseo.poo.projet.artiste.modele;
+
+import java.awt.Color;
+
+public interface Coloriable
+{
+    public Color getCouleur();
+
+    public void setCouleur(Color couleur);
+}
